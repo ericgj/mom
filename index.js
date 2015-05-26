1ad22ac89a6d34ce43fe4b2815edabb2461f08c6
Eric Gjertsen <ericgj72@gmail.com>
2015-05-25 21:19:16 -0400

    cursor: internal rename

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  core: require('./src/core')
+  core: require('./src/core'),
   vdom: require('./src/vdom')
 }
--- src/cursor.js
+++ src/cursor.js
@@ -4,45 +4,43 @@ var mori = require('mori');
 var isArray = require('is-array');
 var identity = function(a){return a;};
 
-module.exports =  Cursor;
-
-function Cursor(getter, setter){
+module.exports = function Cursor_(getter, setter){
   setter = setter || identity;
 
-  function cursor(addr){
-    if (!(this instanceof cursor)) return new cursor(addr);
-    if (arguments.length < 1) return new cursor([]);
+  function Cursor(addr){
+    if (!(this instanceof Cursor)) return new Cursor(addr);
+    if (arguments.length < 1) return new Cursor([]);
     this.base = addr;
     this._listeners = mori.hashMap();
   }
 
-  cursor.prototype.refine =
-  cursor.prototype.child = function(addr){
-    return new cursor( concat(this.base,addr) );
+  Cursor.prototype.refine =
+  Cursor.prototype.child = function(addr){
+    return new Cursor( concat(this.base,addr) );
   }
 
-  cursor.prototype.get =
-  cursor.prototype.value = function(addr){
+  Cursor.prototype.get =
+  Cursor.prototype.value = function(addr){
     return get(this.base, addr);
   }
 
-  cursor.prototype.transact = function(addr, f, tag){
+  Cursor.prototype.transact = function(addr, f, tag){
     return transact(this, this.base, addr, f, tag);
   }
 
-  cursor.prototype.listen = function(key, f){
+  Cursor.prototype.listen = function(key, f){
     this._listeners = mori.assoc(this._listeners, key, f);
     return this;
   }
   
-  cursor.prototype.notify = function(txdata){
-    var c = cursor();  // top-level
+  Cursor.prototype.notify = function(txdata){
+    var c = Cursor();  // top-level
     mori.each(this._listeners, function(_,f){
       f(txdata, c);
     });
   }
   
-  return cursor;
+  return Cursor;
 
   function get(base,addr){
     if (undefined === addr) return get(base, []);
