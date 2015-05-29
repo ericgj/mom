5c92ce02d894e3d6fc168c28d75276a46cdb2b60
Eric Gjertsen <ericgj72@gmail.com>
2015-05-29 11:48:19 -0400

    add vdom.svg builder
    add conventional abbrevations vdom.h == html and vdom.s = svg

--- src/vdom.js
+++ src/vdom.js
@@ -4,8 +4,13 @@ var vdom = module.exports = {
   diff:   require('virtual-dom/diff'),
   patch:  require('virtual-dom/patch'),
   create: require('virtual-dom/create-element'),
-  h:      require('virtual-dom/h'),
+  html:   require('virtual-dom/h'),
+  svg:    require('virtual-dom/virtual-hyperscript/svg'),
   thunk:  require('vdom-thunk'),
   main:   require('./main')
 }
+
+// conventional abbreviations
+vdom.h = vdom.html;  
+vdom.s = vdom.svg;
 
